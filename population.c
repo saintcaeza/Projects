9cec54565f3424418081d0fa8af3a4f35ebb5a6a
Jubathefirst <126524140+Jubathefirst@users.noreply.github.com>
2023-03-09 17:05:29 +0000

    Thu, Mar 9, 2023, 6:05 PM +01:00

--- population.c
+++ population.c
@@ -3,11 +3,12 @@
 
 int main(void) {
     //prompt the user for a starting number of llamas
+    n = get_long("how many llamas are there at the beginning? ")
 
     //prompt the user for a goal number of llamas
 
     //add and subtract every 'year; until we reach the goal number of llamas
 
     //print the goal number of years it took to reach the goal number of llamas
-    
+
 }

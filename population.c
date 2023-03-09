e7ee66ab47d852e9d0cbffb7041c68fcf570c277
Jubathefirst <126524140+Jubathefirst@users.noreply.github.com>
2023-03-09 17:00:44 +0000

    Thu, Mar 9, 2023, 6:00 PM +01:00

---
+++ population.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) {
+    //prompt the user for a starting number of llamas
+
+    //prompt the user for a goal number of llamas
+
+    //add and subtract every 'year; until we reach the goal number of llamas
+
+    //print the goal number of years it took to reach the goal number of llamas
+    
+}

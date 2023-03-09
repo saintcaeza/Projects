fd1f4d0173616f57b73e2c6c63ec26f0d725a5d9
Jubathefirst <126524140+Jubathefirst@users.noreply.github.com>
2023-03-09 17:51:51 +0000

    Thu, Mar 9, 2023, 6:51 PM +01:00

--- population.c
+++ population.c
@@ -20,5 +20,5 @@ do {
 while (n < goal);
 
     //print the goal number of years it took to reach the goal number of llamas
-printf( "It took %l years to reach %l number of llamas." year,goal );
+printf( "It took %l years to reach %l number of llamas.") year,goal;
 }

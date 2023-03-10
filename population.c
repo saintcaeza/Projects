d56fe35cf98bf34aa83c73c349ea7cb14298fda3
Jubathefirst <126524140+Jubathefirst@users.noreply.github.com>
2023-03-10 18:45:45 +0000

    Fri, Mar 10, 2023, 7:45 PM +01:00

--- population.c
+++ population.c
@@ -2,23 +2,23 @@
 #include <cs50.h>
 
 int main(void) {
-    //prompt the user for a starting number of llamas
-long n = get_long("how many llamas are there at the beginning? ");
+        //prompt the user for a starting number of llamas
+    long n = get_long("how many llamas are there at the beginning? ");
 
-    //prompt the user for a goal number of llamas
-long goal = get_long("How many llamas do you want to get? ");
+        //prompt the user for a goal number of llamas
+    long goal = get_long("How many llamas do you want to get? ");
 
-    //add and subtract every 'year; until we reach the goal number of llamas
-long year = 1;
+        //add and subtract every 'year' until we reach the goal number of llamas
+    long year = 1;
 
-do {
-    n = n + ( n / 3);
-    n = n - (n / 3);
-    return n;
-    year++;
- }
-while (n < goal);
+    do {
+        n = n + ( n / 3);
+        n = n - (n / 3);
+        return n;
+        year++;
+     }
+    while (n < goal);
 
-    //print the goal number of years it took to reach the goal number of llamas
-printf( "It took %li years to reach %li number of llamas." year, goal)¬;
+        //print the goal number of years it took to reach the goal number of llamas
+    printf("It took %li years to reach %li number of llamas", year, goal);
 }

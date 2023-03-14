8e75027c0b327405af5c7193bf173965db3392a5
Jubathefirst <126524140+Jubathefirst@users.noreply.github.com>
2023-03-14 11:17:32 +0000

    Tue, Mar 14, 2023, 12:17 PM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -5,8 +5,8 @@ int main(void)
 {
     int height = get_int("How tall should the pyramid be? \n");
 
-    for (int i = 0, i < height, i++)
+    for (int i = 0; i < height; i++)
     {
-        printf("#")
+        printf("#");
     }
 }

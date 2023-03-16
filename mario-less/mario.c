b30e686cbd0f3d09f032f3334a29f7b3c449f8e2
Jubathefirst <126524140+Jubathefirst@users.noreply.github.com>
2023-03-16 12:57:57 +0000

    Thu, Mar 16, 2023, 1:57 PM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -19,7 +19,10 @@ int main(void)
         for (int j = 0; j < n; j++)
         {
             printf("#");
-            n++;
+            if (n < height)
+            {
+                n++;
+            }
         }
         printf("\n");
     }

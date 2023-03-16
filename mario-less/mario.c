bf27b28d079bc0373921c9bbaa22d5098e89355b
Jubathefirst <126524140+Jubathefirst@users.noreply.github.com>
2023-03-16 11:57:01 +0000

    Thu, Mar 16, 2023, 12:57 PM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -5,25 +5,16 @@ int main(void)
 {
     //ask user for a positive integer 'height' of the pyramid
     int height = 0;
-    switch (height)
-    {
-        case (height < 1):
-            get_int("How tall should the pyramid be? ");
-            break;
-        case (height > 8):
-            get_int("How tall should the pyramid be? ");
-            break;
-        default:
-            get_int("How tall should the pyramid be? ");
-            break;
-    }
-    /*
     do
     {
         height = get_int("How tall should the pyramid be? ");
     }
-    while (height <= 8);
-    printf("stored: %i\n", height);
+    while (height < 8);
+    printf("stored: %i\n", height)
+
+    }
+    /*
+    ;
 */
     //create a pyramid using the user input
     /*for (int i = 0; i < height; i++)

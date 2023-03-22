6f5d2d32ed9c09c2cd61d3d260a3d4d5f8ff8a14
Jubathefirst <126524140+Jubathefirst@users.noreply.github.com>
2023-03-22 13:54:27 +0000

    Wed, Mar 22, 2023, 2:54 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -12,20 +12,15 @@ int main(void)
     while(number < 0);
 
     //calculate checksum
-    for(int i = 0; i < number; i++)
-    {
-
-    }
 
     //multiply every other digit by 2, starting with second-to-last digit
     //check for the last digits using modulo
-    for(int i = 100, i < number; i += 100){
+    long checksum;
+    for(long i = 100, i < number; i += 100)
+    {
         checksum = number % i;
-        for(int j = 10; j < number; j += )
-        if(checksum )
     }
-    checksum /= 10;
-
+ printf("checksum: %li\n", checksum);
         //Add those products' digits together
 
         //add the some to the sum of the digits that weren't multiplied by 2

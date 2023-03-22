d2ad9dbf70095e2e8d78f960aab62687321e86c3
Jubathefirst <126524140+Jubathefirst@users.noreply.github.com>
2023-03-22 13:18:14 +0000

    Wed, Mar 22, 2023, 2:18 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    long number;
+    do
+    {
+        number = get_long("Give me a credit card number: ");
+    }
+    while(number < 0);
+
+    for(int i = 0; i < number; i++)
+    {
+        
+    }
+    printf("number: %li\n", number);
 }

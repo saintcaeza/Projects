2a2db31040fdc9de62370ad3f43d8d3182540474
Jubathefirst <126524140+Jubathefirst@users.noreply.github.com>
2023-03-24 13:42:57 +0000

    Fri, Mar 24, 2023, 2:42 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -18,47 +18,15 @@ int main(void)
     long n = 100;
 
     //check for the last digits using modulo
+    //Multiplies every other digit by 2, starting with second-to-last digit and adds those digits together
     for(long i = 100; i <= num * 100; i *= 100)
     {
         digit = (num % n) / d;
-        printf("digit: %li\n", digit);
-        printf("c: %li\n", n);
-        printf("d: %li\n", d);
-        printf("i: %li\n", i);
-
-
-
-
         sum += (digit * 2);
-        printf("sum: %i\n", sum);
         d *= 100;
         n *= 100;
     }
-
-    //Multiplies every other digit by 2, starting with second-to-last digit and adds those digits together
-
-
-
-      /*  if(digit > 9)
-        {
-            //Makes sure whatever is multiplied is a single digit\
-            for(long long j = 10; j < digit; j *= 10)
-            {
-                long long divisor = 10;
-                digit = digit / divisor;
-                return digit;
-            }
-        }
-        else
-        {
-            sum = sum + (digit * 2);
-            return sum;
-        }
-        */
 }
-             //Add those products' digits together
-
-
         //add the some to the sum of the digits that weren't multiplied by 2
 
     //check for length and starting digits

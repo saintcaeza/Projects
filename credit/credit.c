9dffc5625ab3af18e928c6687d8e16e238834074
Jubathefirst <126524140+Jubathefirst@users.noreply.github.com>
2023-03-27 14:25:22 +0000

    Mon, Mar 27, 2023, 3:25 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -81,10 +81,10 @@ int main(void)
     else if ((length == 16) || (length == 13))
     {
         //mastercard
-        int print = num / (10 * 15);
+        long print = num / (10 ** 15);
         printf("number / 10 is: %i. \n", print);
 
-        if ((num / (10 * 15) == 4))
+        if ((num / (10 ** 15) == 4))
         {
             printf("VISA\n");
         }

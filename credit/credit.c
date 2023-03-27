c11a65766b4bb8d244584dff3cacf4886cc62cbb
Jubathefirst <126524140+Jubathefirst@users.noreply.github.com>
2023-03-27 15:00:36 +0000

    Mon, Mar 27, 2023, 4:00 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -65,7 +65,7 @@ int main(void)
     else if (length == 15)
     {
         //american express 34 37
-        if ((num / 10000000000000 == 37) || (num / 10000000000000 == 34))
+        if ((num / 1000000000000 == 37) || (num / 1000000000000 == 34))
         {
             printf("AMEX\n");
         }
@@ -76,11 +76,11 @@ int main(void)
     }
     else if ((length == 16) || (length == 13))
     {
-        //mastercard
         if ((num / 1000000000000000 == 4) || (num / 10000000000000 == 4))
         {
             printf("VISA\n");
         }
+        //mastercard
         else if ((num / 100000000000000  == 51) || (num / 100000000000000 == 53) || (num / 100000000000000 == 54) || (num / 100000000000000 == 55))
         {
             printf("MASTERCARD\n");

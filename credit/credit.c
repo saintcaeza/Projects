a1534282c88a42ad7bb273ebc283e4073b5dd8ab
Jubathefirst <126524140+Jubathefirst@users.noreply.github.com>
2023-03-28 14:13:25 +0000

    Tue, Mar 28, 2023, 3:13 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -23,16 +23,20 @@ int main(void)
     {
         check = (num % n) / d;
         check *= 2;
-
+        printf("check: %li\n", check);
          //Multiplies every other digit by 2, starting with second-to-last digit and adds those digits together
         if(check > 9)
         {
             sum += check % 10;
             sum += check / 10;
+            printf("sum: %i\n", sum);
+
         }
         else
         {
             sum += check;
+            printf("sum: %i\n", sum);
+
         }
         d *= 100;
         n *= 100;
@@ -47,7 +51,11 @@ int main(void)
     for(long i = 10; i < num ; i *= 100)
     {
         altcheck = (num % b) / c;
+        printf("altcheck: %li\n", altcheck);
+
         altsum += altcheck;
+        printf("altsum: %i\n", altsum);
+
         b *= 100;
         c *= 100;
         length ++;
@@ -55,12 +63,13 @@ int main(void)
 
     //add the some to the sum of the checks that weren't multiplied by 2
     sum += altsum;
+    printf("sum: %i\n", sum);
 
     //check for length and starting checks
      //print AMEX, MASTERCARD, VISA or INVALID
     if (sum % 10 != 0)
     {
-        printf("INVALID\n");
+        printf("INVALID default\n");
     }
     else
     {
@@ -73,7 +82,7 @@ int main(void)
             }
             else
             {
-                printf("INVALID\n");
+                printf("INVALID amex\n");
             }
         }
         else if ((length == 16) || (length == 13))
@@ -82,14 +91,18 @@ int main(void)
             {
                 printf("VISA\n");
             }
+            else
+            {
+                printf("INVALID visa\n");
+            }
             //mastercard
-            else if ((num / 100000000000000  == 51) || (num / 100000000000000 == 53) || (num / 100000000000000 == 54) || (num / 100000000000000 == 55))
+            if ((num / 100000000000000  == 51) || (num / 100000000000000 == 53) || (num / 100000000000000 == 54) || (num / 100000000000000 == 55))
             {
                  printf("MASTERCARD\n");
             }
             else
             {
-                printf("INVALID\n");
+                printf("INVALID Mastercard\n");
             }
         }
     }

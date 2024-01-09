5326ea86eb8705feabba8f8fe03faaf8f585a503
Ojabo Julius <126524140+simplyjoba@users.noreply.github.com>
2024-01-09 00:23:50 +0000

    Tue, Jan 9, 2024, 1:23 AM +01:00

---
+++ wk02/scrabble/scrabble.c
@@ -0,0 +1,20 @@
+// players create words to score points, and the number of points is the sum of the point values of each letter in the word
+#include <cs50.h>
+#include <stdio.h>
+
+int score(char a[], char b[]);
+
+int main(void)
+{
+    char player1[] = get_int("Player 1: ");
+    char player2[] = get_int("Player 2: ");
+
+    string points = score(player1, player2);
+}
+
+int score(char a[], char b[])
+{
+    for (int i = 0; )
+    compare player 1 and 2
+    print “Player 1 wins!”, “Player 2 wins!”, or “Tie!”
+}

a4d0d2e6d51f5d74ff20f37f8c606802fe48d89d
Ojabo Julius <126524140+simplyjoba@users.noreply.github.com>
2024-01-11 01:22:41 +0000

    Thu, Jan 11, 2024, 2:22 AM +01:00

--- wk02/scrabble/scrabble.c
+++ wk02/scrabble/scrabble.c
@@ -1,51 +1,79 @@
-// players create words to score points, and the number of points is the sum of the point values of each letter in the word
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-string score(string a[],string b[]);
+char* score(string a,string b);
+
+const int N = 27;
 
 int main(void)
 {
     string player1 = get_string("Player 1: ");
     string player2 = get_string("Player 2: ");
 
-    string points = score(player1, player2);
+    char* points = score(player1, player2);
+    printf("%s", points);
 }
 
-string score(string a[], string b[])
+char* score(string a, string b)
 {
     int n = 0;
-    int sum1;
-    int sum2;
+    int sum1 = 0;
+    int sum2 = 0;
 
     int point[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
-    char alphabet[] = {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z};
+    char alphabet[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
-
-    for (int i = 0: i < 27: i++)
+    //update sum1 with the score of player1
+    for (int i = 0; i < strlen(a); i++)
     {
-        if (alphabet[i] = a[n])
+        //iterates through player1 - 27 times
+        for (int j = 0; i < N; j++)
         {
-            sum1 += point[i];
-            n++
+            a[n] =  toupper(a[n]);
+
+            //determines the index of character 'n' relative to alphabet
+            if (alphabet[j] == a[n])
+            {
+                sum1 += point[j];
+            }
         }
-        if (aplhabet[i] = b[n])
-        {
-            sum2 += point[i];
-        }
+
+        n++;
     }
 
+    //update sum2 with the score of player2
+    for (int i = 0; i < strlen(b); i++)
+    {
+         b[n] =  toupper(b[n]);
+
+        //iterates through player2 - 27 times
+        for (int j = 0; i < N; j++)
+        {
+            //determines the index of character 'n' relative to alphabet
+            if (alphabet[j] == b[n])
+            {
+                sum2 += point[j];
+            }
+        }
+
+        n++;
+    }
+
+    //determines the winner by comparing sums
+  char* win = "No result yet";
     if (sum1 > sum2)
     {
-        printf("Player 1 wins!");
+       win = "Player 1 wins!";
     }
-    else if (sum2 > sum1)
+    else if (sum1 < sum2)
     {
-        printf("Player 2 wins!");
+        win = "Player 2 wins!";
     }
-    else if (sum1 = sum2)
+    else if (sum1 == sum2)
     {
-        printf("Tie!");
+        win = "Tie!";
     }
+    return win;
 }
-

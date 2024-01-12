c8da27c1fb7cb23fe1c0cb88ea86540dd9300e8a
Ojabo Julius <126524140+simplyjoba@users.noreply.github.com>
2024-01-12 18:23:30 +0000

    Fri, Jan 12, 2024, 7:23 PM +01:00

--- wk02/readabililty/readability.c
+++ wk02/readabililty/readability.c
@@ -1,11 +1,22 @@
-// Program calculates the approximate lever required to read some text
+// Program calculates the approximate level required to read some text
 #include <cs50.h>
 #include <ctype.h>
 
-// use the Coleman-Liau index to calculate the grade requred to read text
-    // index = 0.0588 * L - 0.296 * S - 15.8
-    // where L is the average number of letters per 100 words in the text
-    // where S is the average number of sentences per 100 words in the text
+int calculate_grade(string text):
+
+int main(void)
+{
+    string text = (get_string"Input sample text: \n");
+}
+
+int calculate_grade(string text)
+{
+    // use the Coleman-Liau index to calculate the grade requred to read text
+        // index = 0.0588 * L - 0.296 * S - 15.8
+        // where L is the average number of letters per 100 words in the text
+        // where S is the average number of sentences per 100 words in the text
+}
+
 
 
 

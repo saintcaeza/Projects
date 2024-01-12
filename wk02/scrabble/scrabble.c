409e62313a9c1c535be325c6612c1983a2f9249f
Ojabo Julius <126524140+simplyjoba@users.noreply.github.com>
2024-01-12 16:45:04 +0000

    Fri, Jan 12, 2024, 5:45 PM +01:00

--- wk02/scrabble/scrabble.c
+++ wk02/scrabble/scrabble.c
@@ -18,7 +18,6 @@ int main(void)
 
 char* score(string a, string b)
 {
-    int n = 0;
     int sum1 = 0;
     int sum2 = 0;
 
@@ -35,6 +34,8 @@ char* score(string a, string b)
     //iterates through player1 - 27 times
     for (int j = 0; j < N; j++)
     {
+        int n = 0;
+
         a[n]=  toupper(a[n]);
 
         //determines the index of character 'n' relative to alphabet
@@ -57,6 +58,7 @@ char* score(string a, string b)
     } */
     for (int j = 0; j < N; j++)
     {
+        int n = 0;
         b[n] =  toupper(b[n]);
 
         //determines the index of character 'n' relative to alphabet
@@ -65,7 +67,7 @@ char* score(string a, string b)
             printf("%s\n", b);
             sum2 += point[j];
         }
-        
+
         n++;
     }
 

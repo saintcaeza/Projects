687d425c737bffdaae5d5db606d69d64121be180
Ojabo Julius <126524140+simplyjoba@users.noreply.github.com>
2024-01-12 18:15:23 +0000

    Fri, Jan 12, 2024, 7:15 PM +01:00

--- wk02/readabililty/readability.c
+++ wk02/readabililty/readability.c
@@ -1,4 +1,14 @@
 // Program calculates the approximate lever required to read some text
+#include <cs50.h>
+#include <ctype.h>
+
+// use the Coleman-Liau index to calculate the grade requred to read text
+    // index = 0.0588 * L - 0.296 * S - 15.8
+    // where L is the average number of letters per 100 words in the text
+    // where S is the average number of sentences per 100 words in the text
+
+
+
 
 // program should print as output “Grade X” where “X” is the grade level computed
 

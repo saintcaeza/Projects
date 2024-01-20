4d2a244d935b9848d078c5cc3fbe95fdfe9cc29e
Ojabo Julius <126524140+simplyjoba@users.noreply.github.com>
2024-01-20 19:55:22 +0000

    Sat, Jan 20, 2024, 8:55 PM +01:00

--- wk02/caesar.c
+++ wk02/caesar.c
@@ -3,6 +3,15 @@
     // Make sure argument contains only digit characters
     // convert argument to an integer
 
+#include <cs50.h>
+#include <std.io>
+#include <stdlib>
+
+int main(int argc, int arg[v])
+{
+    string plaintext = get_string("plaintext: \n")
+}
+
 //  Get plaintext
 // Encipher
 // print cipher

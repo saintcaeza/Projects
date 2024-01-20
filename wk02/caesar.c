7a52b4226525fe40fd46a2705ef6774649b406fe
Ojabo Julius <126524140+simplyjoba@users.noreply.github.com>
2024-01-20 20:05:49 +0000

    Sat, Jan 20, 2024, 9:05 PM +01:00

--- wk02/caesar.c
+++ wk02/caesar.c
@@ -2,13 +2,17 @@
 #include <cs50.h>
 #include <stdlib>
 
-t
+char* Encipher(int argc, plaintext);
 
 int main(int argc, int arg[v])
 {
     string plaintext = get_string("plaintext: \n")
 }
 
+char* Encipher(int argc, plaintext)
+{
+    
+}
 
 //  Get plaintext
 // Encipher

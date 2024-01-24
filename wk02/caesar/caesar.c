bff92335815a1f8185a585614b918e8edb5a362c
Ojabo Julius <126524140+simplyjoba@users.noreply.github.com>
2024-01-24 16:28:20 +0000

    Wed, Jan 24, 2024, 5:28 PM +01:00

--- wk02/caesar/caesar.c
+++ wk02/caesar/caesar.c
@@ -4,35 +4,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-const int key = 0;
+bool digits(string k);
 
-void get_key(int , string argv[], int keylen);
-
-int main(int argc, char *argv[])
+int main(int argc, string argv[])
 {
     if (argc != 2)
     {
         printf("Error: invalid number of command line arguments. Usage: ./caesar key");
         return 1;
     }
-    get_key(argc, argv, keylen);
-    string plaintext = get_string("plaintext: \n");
+    return 0
+
+
 }
 
-void get_key(int argc, string argv[], int keylen)
-{ // check if key is valid, convert argument to integer
-    if (argc != 2)
-    {
-        if (argv[2] >= 0 && argv[2] <= 9)
-        {
-            key = atoi(argv[1]);
-        }
-    }
-    else
-    {
-        printf("Usage: ./caesar key");
-    }
-}
+bool digits(string k)
+{
 
+}
 // Encipher
 // print cipher

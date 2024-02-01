82a006f50eefebb067ede091d30c98a804db008a
Ojabo Julius <126524140+simplyjoba@users.noreply.github.com>
2024-02-01 20:53:43 +0000

    Thu, Feb 1, 2024, 9:53 PM +01:00

--- wk02/caesar/caesar.c
+++ wk02/caesar/caesar.c
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+cont int N = 2;
+char ALPHABET[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
 bool digits(string k);
 
 int main(int argc, string argv[])
@@ -19,16 +23,53 @@ int main(int argc, string argv[])
 }
 
 bool digits(string k)
-{
-    int len = strlen(string argv1)
+{// Encipher
+    string k = (get_string("Input text: "));
+    int len = strlen(k);
+    int index = 0;
+
+    // iterates through the characters of text
+    for (int i = 0; i < len; i++)
+    {    // convert ASCII characters to alphabetical index
+        if (isalpha(k[i]))
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if ((alphbet[j] || Alphabet[j]) == k[i])
+                {
+                    index += j;
+                    return index;
+                }
+            }
+
+            // shift alphabetical index using formula c[i] = (p[i] + k) % 26
+            int c = (index + key) % 26;
+
+            //convert back to ASCII preserving cases
+            if (64 < k[i] && k[i] < 91)
+            {
+                c += 65;
+                k[i] = c
+            }
+            else if (96 < k[i] && k[i] < 123)
+            {
+                c += 97;
+                k[i] = c;
+
+            }
+
+        }
+    }
 }
 
 /*
     TO DO
     - Get Key
-        - Get commandlinearg and Key
+         - Get commandlinearg and Key
     - Get plaintext
-    - Encipher
-        - shift the position of characters in alphabet by Key position
+    -
+        - shift all alphabets by key preserve case
+            -
+            -
     - Print ciphertext
 */

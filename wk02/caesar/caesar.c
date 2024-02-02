3170d913ef0be6f7332c7ca8ccb0f593050a8115
Ojabo Julius <126524140+simplyjoba@users.noreply.github.com>
2024-02-02 16:02:45 +0000

    Fri, Feb 2, 2024, 5:02 PM +01:00

--- wk02/caesar/caesar.c
+++ wk02/caesar/caesar.c
@@ -4,11 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-cont int N = 2;
+cont int N = 25;
 char ALPHABET[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-bool digits(string k);
+string enciper(string k);
 
 int main(int argc, string argv[])
 {
@@ -22,46 +22,45 @@ int main(int argc, string argv[])
 
 }
 
-bool digits(string k)
+string encipher(string k)
 {// Encipher
-    string k = (get_string("Input text: "));
-    int len = strlen(k);
+    string plaintext = (get_string("Input text: "));
+    char* ciphertext[];
+    int len = strlen(plaintext);
     int index = 0;
+
 
     // iterates through the characters of text
     for (int i = 0; i < len; i++)
     {    // convert ASCII characters to alphabetical index
-        if (isalpha(k[i]))
+        if (isalpha(plaintext[i]))
         {
             for (int j = 0; j < N; j++)
             {
-                if ((alphbet[j] || Alphabet[j]) == k[i])
-                {
+                if ((alphbet[j] || Alphabet[j]) == plaintext[i])
+                { // find's the alphabetical index of a character in text
                     index += j;
                     return index;
                 }
             }
 
             // shift alphabetical index using formula c[i] = (p[i] + k) % 26
-            int c = (index + key) % 26;
+            char c = (index + key) % 26;
 
             //convert back to ASCII preserving cases
             if (64 < k[i] && k[i] < 91)
             {
                 c += 65;
-                k[i] = c
+                ciphertext[i] = c;
             }
             else if (96 < k[i] && k[i] < 123)
             {
                 c += 97;
-                k[i] = c;
-
+                ciphertext[i] = c;
             }
-
         }
     }
 }
-
 /*
     TO DO
     - Get Key

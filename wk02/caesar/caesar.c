995b2e89df13b7ea13bff79d765338b62f74aa23
Ojabo Julius <126524140+simplyjoba@users.noreply.github.com>
2024-04-13 23:06:18 +0000

    Sun, Apr 14, 2024, 12:06 AM +01:00

--- wk02/caesar/caesar.c
+++ wk02/caesar/caesar.c
@@ -8,7 +8,7 @@ const int N = 25;
 char ALPHABET[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-void cipher(string k);
+void cipher(string p);
 
 int main(int argc, string argv[])
 {
@@ -31,61 +31,5 @@ int main(int argc, string argv[])
 
 }
 
-void encipher(string k)
-{// Encipher
-    int key;
-
-    int index = 0;
-
-    string plaintext = (get_string("Input text: "));
-
-    int len = strlen(plaintext);
-
-    char ciphertext[len +1];
-
-    key = atoi(k);
-
-    // iterates through the characters of text
-    for (int i = 0; i < len; i++)
-    {    // convert ASCII characters to alphabetical index
-        if (isalpha(plaintext[i]))
-        {
-            for (int j = 0; j < N; j++)
-            {
-                if ((alphabet[j] || ALPHABET[j]) == plaintext[i])
-                { // find's the alphabetical index of a character in text
-                    index += j;
-                }
-            }
-
-            // shift alphabetical index using formula c[i] = (p[i] + k) % 26
-            char c = (index + key) % 26;
-
-            //convert back to ASCII preserving cases
-            if (64 < k[i] && k[i] < 91)
-            {
-                c += 65;
-                ciphertext[i] = c;
-            }
-            else if (96 < k[i] && k[i] < 123)
-            {
-                c += 97;
-                ciphertext[i] = c;
-            }
-        }
-    }
-
-    ciphertext[len] = '\0';
-    printf("%s", ciphertext);
-}
-/*
-    TO DO
-    - Get Key
-         - Get commandlinearg and Key
-    - Get plaintext
-    -
-        - shift all alphabets by key preserve case
-            -
-            -
-    - Print ciphertext
-*/
+#pseudocode
+#1. 

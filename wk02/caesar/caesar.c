3d66e3a5153b98393514c7b187580da50eb58648
Ojabo Julius <126524140+simplyjoba@users.noreply.github.com>
2024-04-13 23:03:07 +0000

    Sun, Apr 14, 2024, 12:03 AM +01:00

--- wk02/caesar/caesar.c
+++ wk02/caesar/caesar.c
@@ -8,7 +8,7 @@ const int N = 25;
 char ALPHABET[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-void encipher(string k);
+void cipher(string k);
 
 int main(int argc, string argv[])
 {

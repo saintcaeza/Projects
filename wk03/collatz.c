86a41fdbfc44bc7ab6a01a3a989516669db4b9e3
Ojabo Julius <126524140+simplyjoba@users.noreply.github.com>
2024-04-18 16:45:50 +0000

    Thu, Apr 18, 2024, 5:45 PM +01:00

--- wk03/collatz.c
+++ wk03/collatz.c
@@ -9,11 +9,18 @@ int main (void)
     int score = 0;
     int n = get_int("what's the int: \n");
 
-    collatz(n, score);
+    if (n == 1)
+        printf("The number of steps to get to 1 is %d.\n", score);
+    else
+        do
+        {
+            collatz(n, score);
+        } while (n != 1);
+
     printf("The number of steps to get to 1 is %d.\n", score);
 }
 
 void collatz(int n, int score)
 {
-    if 
+
 }

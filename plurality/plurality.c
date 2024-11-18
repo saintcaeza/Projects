ce19af9f88b27f80b8f16a79e462975bda9e22cb
Joba <126524140+simplyjoba@users.noreply.github.com>
2024-11-18 12:36:24 +0000

    Mon, Nov 18, 2024, 1:36 PM +01:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -98,7 +98,9 @@ void print_winner(void)
         sort the votes of each candidate
         if (candidates[i].votes > candidates[i+1].votes)
         {
-            int temp = arr[candidates[i].votes]
+            int temp = candidates[i].votes;
+            candidates[i+1].votes = candidates[i].votes
+
         }
         print the highetst number of votes
 

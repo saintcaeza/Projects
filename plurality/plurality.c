e8cd3e60f10b1fbbdc662efa98f4cf8f66f2676a
Joba <126524140+simplyjoba@users.noreply.github.com>
2024-11-19 11:27:09 +0000

    Tue, Nov 19, 2024, 12:27 PM +01:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -92,7 +92,7 @@ void print_winner(void)
     for (int i = 0; i < candidate_count; i++)
     {
         swap_counter = 0;
-        for (int j = 0; j < candidate_count; j++)
+        for (int j = 0; j < candidate_count-1; j++)
         {
 
             if (candidates[j].votes > candidates[j+1].votes)
